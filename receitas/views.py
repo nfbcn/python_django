ce5f6d393d021b2534533be102de1d3a8d0cd83a
nikolas <nikolasads@gmail.com>
2022-07-25 07:19:16 -0300

    atualização

--- receitas/views.py
+++ receitas/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-from django.http      import HttpResponse
+#from .models import Receita
+
 
 def index(request):
-    return HttpResponse('<h1>receitas</h1>')
+
+   # receitas = Receita.objects.all()
+    #print(receitas)
+    receitas = {
+        1:'Lasanha',
+        2:'Sopa de legumes',
+        3:'Sorvete',
+        4:'Bolo de Chocolate'
+    }
+    dados = {
+        'nome_das_receitas': receitas
+    }
+    return render(request,'index.html', dados)
+
+def receita (request):
+    return render(request, 'receita.html')
