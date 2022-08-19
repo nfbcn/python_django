def7755125c3a376a7025bb9c2a7ddc39767ceff
Nikolas Nascimento <nikolas.nascimento@neurotech.com.br>
2022-08-19 00:17:26 -0300

    falta pouco

--- receitas/views.py
+++ receitas/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render
-#from .models import Receita
+from .models import Receita
 
 
 def index(request):
 
-   # receitas = Receita.objects.all()
+    receitas = Receita.objects.all()
     #print(receitas)
-    receitas = {
-        1:'Lasanha',
-        2:'Sopa de legumes',
-        3:'Sorvete',
-        4:'Bolo de Chocolate'
-    }
+    
     dados = {
-        'nome_das_receitas': receitas
+        'receitas': receitas
     }
     return render(request,'index.html', dados)
 
-def receita (request):
+def receita (request, receita_id):
     return render(request, 'receita.html')
